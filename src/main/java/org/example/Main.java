7490ad7afff8f61d9b96690b8c02371e5b69a3c1
Tymcha <andriyy29.timchuk@gmail.com>
2023-11-04 09:16:03 +0200

    Open Chrome

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,12 @@
 package org.example;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
     public static void main(String[] args) {
-        WebDriver driver;
+        System.setProperty("webdrive.chrome.driver", "D:\\WebDriver\\chromedriver-win64\\chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com.ua");
     }
 }
